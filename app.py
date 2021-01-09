a6bb339d0ca39e90f6b8f4680c2223dcfd4f19b0
dkzks44 <dkzks44@users.noreply.github.com>
2021-01-09 17:18:08 +0900

    python 내부 검색
    python 내에서 검색하고 결과 도출 가능

--- app.py
+++ app.py
@@ -1,55 +1,34 @@
-from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 
-app = Flask(__name__)
+# URL을 읽어서 HTML를 받아오고,
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta_ninework  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 
-@app.route('/memo', methods=['POST'])
-def post_article():
-    # 1. 클라이언트로부터 데이터를 받기
-    url_receive = request.form['url_give']  # 클라이언트로부터 url을 받는 부분
-    comment_receive = request.form['comment_give']  # 클라이언트로부터 comment를 받는 부분
+url = 'https://www.daangn.com/search/'
+plus_url = input('Wanna search?')
+full_url = url + plus_url
 
-    # 2. meta tag를 스크래핑하기
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+# https://www.daangn.com/search/%EA%B0%A4%EB%9F%AD%EC%8B%9C%ED%83%AD
 
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
+data = requests.get(full_url, headers=headers)
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+# select를 이용해서, tr들을 불러오기
+products = soup.select('#flea-market-wrap > article')
+#flea-market-wrap > article:nth-child(4) > a > div.card-photo > img
+#flea-market-wrap > article:nth-child(4) > a > div.article-info > div > span.article-title
+#flea-market-wrap > article:nth-child(3) > a > div.article-info > div
+#flea-market-wrap > article:nth-child(4) > a > div.article-info > p.article-price
 
-    url_title = og_title['content']
-    url_description = og_description['content']
-    url_image = og_image['content']
-
-    article = {'url': url_receive, 'title': url_title, 'desc': url_description, 'image': url_image,
-               'comment': comment_receive}
-
-    # 3. mongoDB에 데이터를 넣기
-    db.articles.insert_one(article)
-
-    return jsonify({'result': 'success'})
-
-
-@app.route('/memo', methods=['GET'])
-def read_articles():
-    # 1. mongoDB에서 _id 값을 제외한 모든 데이터 조회해오기 (Read)
-    result = list(db.articles.find({}, {'_id': 0}))
-    # 2. articles라는 키 값으로 article 정보 보내주기
-    return jsonify({'result': 'success', 'articles': result})
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+# movies (tr들) 의 반복문을 돌리기
+for product in products:
+    # movie 안에 a 가 있으면,
+    a_tag = product.select_one('div.article-info > div > span.article-title')
+    if a_tag is not None:
+        product_img = product.select_one('div.card-photo > img')['src']
+        name = product.select_one('article > a > div.article-info > div > span.article-title').text
+        price = product.select_one('article > a > div.article-info > p.article-price').text  # td 태그 사이의 텍스트를 가져오기
+        print(product_img, name, price)
