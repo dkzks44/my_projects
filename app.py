29f4be6a75b6edac9049af67f681acc64ffd96b9
dkzks44 <dkzks44@users.noreply.github.com>
2021-01-21 06:50:08 +0900

    Update_4
    중고나라 크롤링 / 체크박스 전체선택 / 제목 클릭시 홈으로 이동 기능 추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, jsonify
 
@@ -16,20 +17,21 @@ def search_word():
     url = 'https://www.daangn.com/search/'
     plus_url = search_receive
     more_url = '/more/flea_market?page='
+    full_url = url + plus_url + more_url
+
     for page_number in range(1, 5):
         full_url = url + plus_url + more_url + str(page_number)
     first_url = url + plus_url
-    product_detail = 'https://www.daangn.com'
 
     data = requests.get(full_url, headers=headers)
     soup = BeautifulSoup(data.text, 'html.parser')
 
-    data2 = requests.get(first_url, headers=headers)
+    products = soup.select('#flea-market-wrap > article')
+    data2 = requests.get(first_url,headers=headers)
     soup2 = BeautifulSoup(data2.text, 'html.parser')
 
-    # # flea-market-wrap > article
-    # #image-slider > div > div
-    # flea-market-wrap > article:nth-child(3) > a.href
+    # >> # flea-market-wrap > article
+    # flea-market-wrap > article:nth-child(2) > a > div.article-info > p.article-region-name
 
     products = soup.select('article')
     first_products = soup2.select('#flea-market-wrap > article')
@@ -40,11 +42,10 @@ def search_word():
         if a_tag is not None:
             img1 = first_product.select_one('div.card-photo > img')['src']
             name1 = first_product.select_one('article > a > div.article-info > div > span.article-title').text
-            price1 = first_product.select_one('article > a > div.article-info > p.article-price').text
-            url1 = first_product.select_one('article > a.href').string
-            product_domain1 = product_detail + url1
+            price1 = first_product.select_one('article > a > div.article-info > p.article-price').text  # td 태그 사이의 텍스트를 가져오기
+            location1 = first_product.select_one('article > a > div.article-info > p.article-region-name').text
 
-            card1 = {'product_url': product_domain1, 'product_img': img1, 'product_name': name1, 'product_price': price1}
+            card1 = {'product_url': first_url, 'product_img': img1, 'product_name': name1, 'product_price': price1, 'product_location': location1}
             cards.append(card1)
 
     for product in products:
@@ -52,13 +53,34 @@ def search_word():
         if a_tag is not None:
             img = product.select_one('div.card-photo > img')['src']
             name = product.select_one('article > a > div.article-info > div > span.article-title').text
-            price = product.select_one('article > a > div.article-info > p.article-price').text
-            url2 = product.select_one('article > a.href').string
-            product_domain2 = product_detail + url2
-
-
-            card = {'product_url': product_domain2, 'product_img': img, 'product_name': name, 'product_price': price}
+            price = product.select_one('article > a > div.article-info > p.article-price').text  # td 태그 사이의 텍스트를 가져오기
+            location = product.select_one('article > a > div.article-info > p.article-region-name').text
+            card = {'product_url': full_url, 'product_img': img, 'product_name': name, 'product_price': price, 'product_location': location}
             cards.append(card)
+
+    data_jg = requests.post("https://search-api.joongna.com/v25/search/product", json={
+        "filter": {"categoryDepth": 0, "categorySeq": 0,
+                   "dateFilterParameter": {"sortEndDate": None, "sortStartDate": None}, "flawedYn": 0,
+                   "fullPackageYn": 0, "limitedEditionYn": 0, "maxPrice": 2000000000, "minPrice": 0,
+                   "productCondition": -1, "tradeType": 0}, "osType": 2, "searchQuantity": 40,
+        "searchStartTime": "2021-01-20 15:53:44", "searchWord": search_receive, "sort": "RECOMMEND_SORT", "startIndex": 0,
+        "productFilter": "APP"})
+
+    soup_jg = BeautifulSoup(data_jg.text, 'html.parser')
+    a = soup_jg.text
+    product_data = json.loads(a)
+
+    for i in range(0, 40):
+        product_imgae_jg = product_data["data"]["items"][i]["detailImgUrl"]
+        product_title_jg = product_data["data"]["items"][i]["title"]
+        product_price_jg = product_data["data"]["items"][i]["price"]
+        product_location_jg = product_data["data"]["items"][i]["mainLocationName"]
+        # product_url_num_jg = product_data["data"]["items"][i]["seq"]
+        # product_url_jg = 'https://m.joongna.com/product-detail/' + product_url_num_jg.var()
+
+        card_jg = {'product_img': product_imgae_jg, 'product_name': product_title_jg,
+                   'product_price': product_price_jg, 'product_location': product_location_jg}
+        cards.append(card_jg)
 
     return jsonify({'result': 'success', 'searching_info': cards})
 
